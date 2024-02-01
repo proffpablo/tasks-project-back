a9c348ce4048c5bc5f3835c3dc5a4568da9acfc8
Pablo Andres Martinez Rejas <91680362+proffpablo@users.noreply.github.com>
2024-02-01 17:44:48 -0300

    Update auth.controller.js
    sdfascsdfd

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -87,7 +87,7 @@ export const profile = async (req, res) => {
 
 export const verifyToken = async (req, res) => {
   const{token} = req.cookies;
-  console.log('ESTE', req)
+  console.log('ESTE', req.cookies)
   if(!token) return res.status(401).json({ message: "Unauthorized" });
 
   jwt.verify(token, TOKEN_SECRET, async (err, user) => {
