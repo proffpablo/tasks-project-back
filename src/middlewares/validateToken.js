9e6a15953efb98dc9b9a3157347b6e4b14011758
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-05 11:21:54 -0300

    log headers

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const authRequired = (req, res, next) => {
+    console.log("ACA" + req.headers);
     const {token} = req.headers.cookie.split("; ").find(cookie => cookie.startsWith("token=")).slice(6);
     if (!token) return res.status(401).json({ message: "No token, authorization denied" });
 
