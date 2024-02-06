15519f0a9ded0029eb0e132a865f40866068a46a
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-06 08:48:55 -0300

    token verification 2

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -34,7 +34,7 @@ export const register = async (req, res) => {
     }
 
     res.cookie('token', token, cookieOption);
-    console.log("\nCookie seteada con el token: ");
+    console.log("\nCookie seteada con el token: " + res.cookie);
     res.json({
       id: userSaved._id,
       username: userSaved.username,
