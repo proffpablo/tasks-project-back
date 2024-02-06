79d6fd265874ca76b6b1f53ab948b6c69294f978
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-06 10:59:10 -0300

    db config

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log(">>> DB is connected");
     } catch(error) {
         console.log(error);
