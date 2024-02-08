4bad20a77e0ad93df69f06891d6f76c70dc7519b
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-07 22:29:56 -0300

    cors configuration 4

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         'http://localhost:5173',
+        'http://localhost:5173/',
         'http://localhost:5173/login',
         'http://localhost:5173/register',
         'http://localhost:5173/tasks',
