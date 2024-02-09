cba408d8503eb2c47d26f9f55ba15e886ea26922
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-08 23:27:34 -0300

    console.log

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -8,9 +8,13 @@ export const authRequired = (req, res, next) => {
     const authorization = req.get('Authorization');
     let token = '';
 
+    console.log("\n\tAuthorization antes del if: " + authorization)
+
     if (authorization && authorization.toLowerCase().startsWith('bearer')) {
         token = authorization.substring(7);
     }
+
+    console.log("\n\ntoken luego del if: " + token);
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return res.status(403).json({ message: "Invalid token" });
