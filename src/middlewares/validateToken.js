e16a6e9f98f540e744ba2efac3002e03402655ab
proffpablo <pabloandresmartinezrejas@gmail.com>
2024-02-09 11:07:57 -0300

    next when null

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -6,15 +6,12 @@ dotenv.config();
 export const authRequired = (req, res, next) => {
 
     const authorization = req.get('Authorization');
+    if(authorization = null) next();
     let token = '';
-
-    console.log("\n\tAuthorization antes del if: " + authorization)
 
     if (authorization && authorization.toLowerCase().startsWith('bearer')) {
         token = authorization.substring(7);
     }
-
-    console.log("\n\ntoken luego del if: " + token);
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) return res.status(403).json({ message: "Invalid token" });
